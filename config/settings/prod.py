91619057e298b9e53fa71c34f9c56739626859a9
강재경 <smile.bear24@gmail.com>
2022-07-02 14:41:58 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,7 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.124.147.130']
-
-#STATIC_ROOT = BASE_DIR / 'static/'
-
-#STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
 
 #DEBUG = False
