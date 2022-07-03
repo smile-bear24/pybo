6fc58ee011c33b08044f05741fbdcf97e79fb72e
강재경 <smile.bear24@gmail.com>
2022-07-03 23:51:03 +0900

    로깅 테스트

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
 
 from ..models import Question
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
-    3/0
     """
     pybo 목록 출력
     """
+
+    logger.info("INFO 레벨로 출력")
     # 입력 인자
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')       # 검색어
